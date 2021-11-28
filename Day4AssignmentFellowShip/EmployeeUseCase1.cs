5061dd6ea68fd1ecbedbfc669db9f5c1dfe334d6
Wpaswan <dablumcapaswan@gmail.com>
2021-11-27 22:15:43 -0800

    Uc1 Attendance program of Emp_wage added

---
+++ Day4AssignmentFellowShip/EmployeeUseCase1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4AssignmentFellowShip
+{
+    internal class EmployeeUseCase1
+    {
+        public void EmpWage1()
+        {
+
+            int isPresent = 1;
+            Random random = new Random();
+            int randominput = random.Next(0, 2);
+
+            if (randominput == isPresent)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day4AssignmentFellowShip/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Day4AssignmentFellowShip;
+Console.WriteLine("\tEnter any key to continiue and n to stop");
+char cho=Convert.ToChar(Console.ReadLine());
+while (cho != 'n')
+{
+    Console.WriteLine("\t((((((((Please Select   )))))))");
+    Console.WriteLine("\\t(((((((1.Employ Wage Uc1)))))))");
+    
+    int ch = Convert.ToInt32(Console.ReadLine());
+    switch (ch)
+    {
+        case 1:
+            EmployeeUseCase1 e1 = new EmployeeUseCase1();
+            e1.EmpWage1();
+            break;
+    }
+    break;
+    Console.WriteLine("\tEnter any key to continiue and n to stop");
+    cho = Convert.ToChar(Console.ReadLine());
+}
