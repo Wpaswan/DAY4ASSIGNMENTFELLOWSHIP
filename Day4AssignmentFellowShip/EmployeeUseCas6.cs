558240dc329ec8b35d762e25e71598dc61e202a4
Wpaswan <dablumcapaswan@gmail.com>
2021-11-28 01:15:00 -0800

    Uc6 Employee wage addedgit add .

---
+++ Day4AssignmentFellowShip/EmployeeUseCas6.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4AssignmentFellowShip
+{
+    internal class EmployeeUseCas6
+    {
+        public const int is_Full_Time = 1;
+        public const int is_Part_Time = 2;
+        public const int RatePerHrs = 20;
+        public const int Max_Working_Days = 20;
+        public const int Max_Working_Hrs = 100;
+        public void input4()
+        {
+            int empWage = 0;
+            int empHrs = 0;
+            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalDays = 1;
+            string typeOfEmployee = "";
+            string name = "XyZ";
+
+
+            Random employee = new Random();
+            while (totalEmpHrs<=Max_Working_Hrs && totalDays<=Max_Working_Days)
+            {
+                int randomInput = employee.Next(0, 3);
+
+                switch (randomInput)
+                {
+                    case is_Full_Time:
+                        empHrs = 8;
+                        typeOfEmployee = "Full Time";
+                        break;
+
+                    case is_Part_Time:
+                        empHrs = 4;
+                        typeOfEmployee = "Part Time";
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        typeOfEmployee = "Absent";
+                        break;
+                }
+                totalEmpHrs = totalEmpHrs + empHrs;
+                empWage = RatePerHrs * empHrs;
+                totalEmpWage = RatePerHrs * totalEmpHrs;
+                Console.WriteLine("{0}Emplyee of day: {1}wage is {2}", typeOfEmployee, totalDays, empWage);
+                totalEmpWage = totalEmpWage + empWage;
+                totalDays++;
+            }
+            Console.WriteLine("Employee wage for {0} day is : {1}", Max_Working_Days, totalEmpWage);
+            Console.ReadLine();
+        }
+    }
+}
